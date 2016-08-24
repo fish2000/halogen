acd7d2e9fd200413fb0b907d5e13069a648ef9a2
Alexander Böhn <fish2000@gmail.com>
2016-08-24 17:12:02 -0400

    JUST ADD GENERATORS

--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize([Extension('hal.api', ['hal/api.pyx'],
+    ext_modules=cythonize([
+        Extension('hal.api', ['hal/api.pyx', 'generators/lesson_15_generators.cpp'],
         extra_link_args=[
-            '-lHalide'
-        ],
+            '-lHalide'],
         extra_compile_args=[
             '-Wno-unused-function',
             '-Wno-unneeded-internal-declaration',
             '-O3',
+            '-fno-rtti',
             '-funroll-loops',
             '-mtune=native',
             '-std=c++1z',
-            '-stdlib=libc++'
-        ])
+            '-stdlib=libc++']
+        )
     ])
 )
